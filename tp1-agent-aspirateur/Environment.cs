9246a2915dff7789e8fa72cfc95d8776deb560b1
Olivier <olivier.tinh@gmail.com>
2019-01-29 19:56:49 -0500

    Added cells with inner state

--- tp1-agent-aspirateur/Environment.cs
+++ tp1-agent-aspirateur/Environment.cs
@@ -4,19 +4,11 @@ using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media.Imaging;
-using System.Windows.Threading;
 
 namespace tp1_agent_aspirateur
 {
     public class Environment
     {
-        private enum Object
-        {
-            DUST,
-            JEWEL,
-            DUST_AND_JEWEL
-        }
-
         // Coordonnées de la grille
         private const int MIN_X = 0;
         private const int MAX_X = 9;
@@ -24,76 +16,120 @@ namespace tp1_agent_aspirateur
         private const int MAX_Y = 9;
 
         // Chance d'apparaître chaque tour en %
-        private const int DUST_SPAWN_CHANCE = 10;
+        private const int DUST_SPAWN_CHANCE = 50;
         private const int JEWEL_SPAWN_CHANCE = 5;
 
+        // La grille et son affichage
         private Thread thread;
-        private Grid grid;
+        private readonly Grid gridDisplay;
+        private Cell[,] grid = new Cell[MAX_X + 1, MAX_Y + 1];
 
-        private static readonly Random rnd = new Random();
-        private static readonly object syncLock = new object();
+        // Variables pour gérer l'aléatoire
+        private static readonly Random Rnd = new Random();
+        private static readonly object SyncLock = new object();
 
-        private int performance { get; set; }
-
-        public Environment(Grid grid)
+        public Environment(Grid gridDisplay)
         {
-            this.grid = grid;
+            this.gridDisplay = gridDisplay;
         }
 
+        // Initialise les cellules de la grille et lance le thread de l'environnement
         public void start()
         {
+            initGrid();
             thread = new Thread(update);
             thread.SetApartmentState(ApartmentState.STA);
             thread.Start();
         }
 
+        // Boucle lancée à chaque tour, avec 500ms de délai entre chacun de ces tours
         private void update()
         {
-            var i = 0;
+            var turn = 0;
             while (true)
             {
                 generateDust();
                 generateJewel();
-                Debug.WriteLine($"Turn: {++i}");
+                Debug.WriteLine($"Turn: {++turn}");
                 Thread.Sleep(500);
             }
+        }
+
+        private void initGrid()
+        {
+            for (var i = 0; i < MAX_X; ++i)
+            for (var j = 0; j < MAX_Y + 1; ++j)
+                grid[i, j] = new Cell(Cell.State.EMPTY);
         }
 
         private void generateDust()
         {
             if (random(0, 100) < DUST_SPAWN_CHANCE)
             {
-                addToGrid(Object.DUST);
+                generate(Cell.State.DUST);
                 Debug.WriteLine("Dust has been generated!");
             }
         }
 
         private void generateJewel()
         {
-            if (random(0, 100) > 100 - JEWEL_SPAWN_CHANCE)
+            if (random(0, 100) < JEWEL_SPAWN_CHANCE)
             {
-                addToGrid(Object.JEWEL);
+                generate(Cell.State.JEWEL);
                 Debug.WriteLine("Jewel has been generated!");
             }
         }
 
-        private void addToGrid(Object o)
+        //TODO: nettoyer ce spaghetti code
+        private void generate(Cell.State cell)
         {
             Application.Current.Dispatcher.Invoke(() =>
             {
                 var x = random(MIN_X, MAX_X);
                 var y = random(MIN_Y, MAX_Y);
-                var image = getImage(o);
-                setCoordinates(image, x, y);
-                grid.Children.Add(image);
+                var sprite = getSprite(cell);
+
+                switch (cell)
+                {
+                    case Cell.State.DUST:
+
+                        switch (grid[x, y].state)
+                        {
+                            case Cell.State.EMPTY:
+                                grid[x, y].state = Cell.State.DUST;
+                                display(sprite, x, y);
+                                break;
+                            case Cell.State.JEWEL:
+                                grid[x, y].state = Cell.State.DUST_AND_JEWEL;
+                                display(getSprite(Cell.State.DUST_AND_JEWEL), x, y);
+                                break;
+                        }
+
+                        break;
+                    case Cell.State.JEWEL:
+
+                        switch (grid[x, y].state)
+                        {
+                            case Cell.State.EMPTY:
+                                grid[x, y].state = Cell.State.JEWEL;
+                                display(sprite, x, y);
+                                break;
+                            case Cell.State.DUST:
+                                grid[x, y].state = Cell.State.DUST_AND_JEWEL;
+                                display(getSprite(Cell.State.DUST_AND_JEWEL), x, y);
+                                break;
+                        }
+
+                        break;
+                }
             });
         }
 
-        private Image getImage(Object o)
+        private Image getSprite(Cell.State state)
         {
             var image = new Image();
             var bitmapImage = new BitmapImage();
-            var uri = o == Object.DUST ? "images/dust.jpg" : "images/jewels.jpg";
+            var uri = state == Cell.State.DUST ? "images/dust.jpg" : "images/jewels.jpg";
 
             bitmapImage.BeginInit();
             bitmapImage.UriSource = new Uri(uri, UriKind.Relative);
@@ -103,17 +139,18 @@ namespace tp1_agent_aspirateur
             return image;
         }
 
-        private void setCoordinates(UIElement image, int x, int y)
+        private void display(UIElement sprite, int x, int y)
         {
-            Grid.SetColumn(image, x);
-            Grid.SetRow(image, y);
+            Grid.SetColumn(sprite, x);
+            Grid.SetRow(sprite, y);
+            gridDisplay.Children.Add(sprite);
         }
 
         private static int random(int min, int max)
         {
-            lock (syncLock)
+            lock (SyncLock)
             {
-                return rnd.Next(min, max + 1);
+                return Rnd.Next(min, max + 1);
             }
         }
     }
--- tp1-agent-aspirateur/MainWindow.xaml.cs
+++ tp1-agent-aspirateur/MainWindow.xaml.cs
@@ -1,42 +1,16 @@
-﻿using System.ComponentModel;
-using System.Diagnostics;
-using System.Threading;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Threading;
-
-namespace tp1_agent_aspirateur
+﻿namespace tp1_agent_aspirateur
 {
     public partial class MainWindow
     {
-        internal static MainWindow main;
-
         public MainWindow()
         {
             InitializeComponent();
-
-            main = this;
 
             var environment = new Environment(RoomGrid);
             environment.start();
 
             var agent = new Agent();
             agent.start();
-        }
-
-        // Alternative pour corriger le problème de thread en appelant cette fonction 
-        // depuis Environment.cs qui ne marche pas non plus
-        public void display(Image image)
-        {
-            if (Dispatcher.CheckAccess())
-            {
-                Application.Current.Dispatcher.Invoke(DispatcherPriority.Normal,
-                    new ThreadStart(() => { RoomGrid.Children.Add(image); }));
-            }
-            else
-            {
-                Debug.WriteLine("doesn't have access");
-            }
         }
     }
 }
